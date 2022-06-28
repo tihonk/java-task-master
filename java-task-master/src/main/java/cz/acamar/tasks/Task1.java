d7e545bd31d39c59630bbfa518a6e2445a17704b
Kasko <t.kasko@itransition.com>
2022-06-28 09:18:15 +0300

    Initial commit

---
+++ java-task-master/src/main/java/cz/acamar/tasks/Task1.java
@@ -0,0 +1,27 @@
+package cz.acamar.tasks;
+
+import static java.util.regex.Pattern.compile;
+
+import java.util.regex.Matcher;
+
+public class Task1 {
+
+    /**
+     * Task 1. Length of the last word.
+     * <p>
+     * Given a string s consisting of some words separated by some number of spaces,
+     * return the length of the last word in the string.
+     * <p>
+     * A word is a maximal substring consisting of non-space characters only.
+     * <p>
+     * Example input: "Hello World"
+     * Expected output: 5
+     *
+     * @param str - a string s consisting of some words separated by some number of spaces.
+     * @return - the length of the last word in the string.
+     */
+    public int lengthOfLastWord(String str) {
+        Matcher matcher = compile("(\\S+)\\s*$").matcher(str);
+        return matcher.find() ? matcher.group().trim().length() : 0;
+    }
+}
